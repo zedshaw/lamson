26bb91e832d07894a2651f18e36ff9b040cd00d6
Zed A. Shaw <zedshaw@zedshaw.com>
2013-03-05 16:17:42 -0800

    Version bump for release.

--- lamson/version.py
+++ lamson/version.py
@@ -1,2 +1,2 @@
-VERSION={'version': '1.3.2', 'rev': ['0ffa23eac',
-'0ffa23eacf9ff8e37ebb86f30164e6df67f93019']}
+VERSION={'version': '1.3.3', 'rev': ['7b45586c',
+'7b45586c8963492b37d792aefb10fc62e334e458']}
